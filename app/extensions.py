0ba5b84d240b13a238df8f4037fc8a5dff4a0750
Yashtripathi6108 <Yashtripathi6108>
2024-09-19 10:31:51 +0530

    another commit

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-mongo_uri = 'mongodb+srv://yashansh24:yash1234@cluster0-shard-00-00.u4rqi.mongodb.net:27017,cluster0-shard-00-01.u4rqi.mongodb.net:27017,cluster0-shard-00-02.u4rqi.mongodb.net:27017/?ssl=true&replicaSet=atlas-qn2nv7-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
+mongo_uri = 'mongodb://yashansh24:yash1234@cluster0-shard-00-00.u4rqi.mongodb.net:27017,cluster0-shard-00-01.u4rqi.mongodb.net:27017,cluster0-shard-00-02.u4rqi.mongodb.net:27017/?ssl=true&replicaSet=atlas-qn2nv7-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
 client = MongoClient(mongo_uri)
 db = client['github_events']
 collection = db['events']
