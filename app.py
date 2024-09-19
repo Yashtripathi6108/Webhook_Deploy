4e92f8a093519def86e4190f1a1e7a62e726ae34
Yashtripathi6108 <Yashtripathi6108>
2024-09-19 11:04:24 +0530

    another commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 app = create_app()
 
-CORS(app, origins=["https://66ebabad2ffe93169d4aa107--fastidious-platypus-481cda.netlify.app/"])
+CORS(app, origins=["https://66ebabad2ffe93169d4aa107--fastidious-platypus-481cda.netlify.app"])
 
 @app.route('/')
 def greetings() -> str:
